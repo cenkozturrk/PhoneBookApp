ac7d05f6dbdeda26bbd9d1ef6ab145fa0c90fde8
Cenk Öztürk <cenk.ozturk@mikro.com.tr>
2021-12-17 14:59:22 +0300

    The business layer was created for writing business rules. Two separate files (abstract , service) were created. Classes named PersonService and IPersonService were created and methods were added, and ctors were also created.

---
+++ PhoneBookApp.Business/Abstract/IPersonService.cs
@@ -0,0 +1,16 @@
+﻿using PhoneBook.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBookApp.Business.Abstract
+{
+    public interface IPersonService
+    {
+        List<Person> GetAllPerson();
+        Person GetPersonById(int id);
+        Person CreatePerson(Person person);
+        Person UpdatePerson(Person person);
+        void DeletePerson(int id);
+    }
+}
---
+++ PhoneBookApp.Business/Services/PersonService.cs
@@ -0,0 +1,45 @@
+﻿using PhoneBook.Entities;
+using PhoneBookApp.Business.Abstract;
+using PhoneBookApp.DataAccess.Abstract;
+using PhoneBookApp.DataAccess.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBookApp.Business.Services
+{
+    public class PersonService : IPersonService
+    {
+        private IPersonRepository _personRepository;
+
+        public PersonService()
+        {
+            _personRepository = new PersonRepository();
+        }
+
+        public Person CreatePerson(Person person)
+        {
+            return _personRepository.CreatePerson(person);
+        }
+
+        public void DeletePerson(int id)
+        {
+            _personRepository.DeletePerson(id);
+        }
+
+        public List<Person> GetAllPerson()
+        {
+            return _personRepository.GetAllPerson();
+        }
+
+        public Person GetPersonById(int id)
+        {
+            return _personRepository.GetPersonById(id);
+        }
+
+        public Person UpdatePerson(Person person)
+        {
+            return _personRepository.UpdatePerson(person);
+        }
+    }
+}
