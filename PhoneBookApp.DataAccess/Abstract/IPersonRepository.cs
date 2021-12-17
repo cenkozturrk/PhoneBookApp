93a178cc2683717483c71761cd7d832bbac0b5a6
Cenk Öztürk <cenk.ozturk@mikro.com.tr>
2021-12-17 12:06:47 +0300

    The abstract file was created inside the data layer and its interfaces were defined.

---
+++ PhoneBookApp.DataAccess/Abstract/IPersonRepository.cs
@@ -0,0 +1,20 @@
+﻿using PhoneBook.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBookApp.DataAccess.Abstract
+{
+    public interface IPersonRepository
+    {
+        List<Person> GetAllPerson();
+
+        Person GetHotelById(int id);
+
+        Person CreatePerson(Person hotel);
+
+        Person UpdatePerson(Person person);
+
+        void DeleteHotel(int id);
+    }
+}
