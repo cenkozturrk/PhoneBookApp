e4867b07a4d12b09c67782799e1316ff718585af
cenk.ozturk <cenk.ozturk@MIKRONB032>
2021-12-17 10:10:40 +0300

    Add project files.

---
+++ PhoneBook.Entities/PhoneBook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PhoneBook.Entities
+{
+    public class PhoneBook
+    {   
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string Surname { get; set; }
+
+
+        public string Company { get; set; }
+
+    }
+}
---
+++ PhoneBookApp.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PhoneBookApp.DataAccess
+{
+    public class Class1
+    {
+    }
+}
