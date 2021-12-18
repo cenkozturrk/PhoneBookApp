d58823fe5facde22b3e44aac9d2abb5016722195
Cenk Öztürk <cenk.ozturk@mikro.com.tr>
2021-12-18 18:10:09 +0300

    The data layer part of the contact class has been written.

---
+++ PhoneBookApp.DataAccess/Abstract/IContactRepository.cs
@@ -0,0 +1,15 @@
+﻿using PhoneBookApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBookApp.DataAccess.Abstract
+{
+    public interface IContactRepository
+    {
+        List<Contact> GetAllContact();
+        Contact CreateContact(Contact contact);
+        Contact UpdateContact(Contact contact);
+
+    }
+}
---
+++ PhoneBookApp.DataAccess/Services/ContactRepository.cs
@@ -0,0 +1,41 @@
+﻿using PhoneBookApp.DataAccess.Abstract;
+using PhoneBookApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhoneBookApp.DataAccess.Services
+{
+    public class ContactRepository : IContactRepository
+    {
+        public Contact CreateContact(Contact contact)
+        {
+            using (var phoneDbContact = new PhoneDbContext())
+            {
+                phoneDbContact.Contacts.Add(contact);
+                phoneDbContact.SaveChanges();
+                return contact;
+
+            }
+        }
+
+        public List<Contact> GetAllContact()
+        {
+           using(var phoneDbContact = new PhoneDbContext())
+            {
+                return phoneDbContact.Contacts.ToList();
+            }
+        }
+
+        public Contact UpdateContact(Contact contact)
+        {
+            using(var phoneDbContext = new PhoneDbContext())
+            {
+                phoneDbContext.Contacts.Update(contact);
+                phoneDbContext.SaveChanges();
+                return contact;
+            }
+        }
+    }
+}
