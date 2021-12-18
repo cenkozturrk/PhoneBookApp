7dde7d4da5f2b2def2ce4e5aa344d644372bd950
Cenk Öztürk <cenk.ozturk@mikro.com.tr>
2021-12-18 17:52:17 +0300

    Fluent validation rules have been added to the system.

---
+++ PhoneBookApp.Business/Validators/ContactValidator.cs
@@ -0,0 +1,69 @@
+﻿using FluentValidation;
+using PhoneBookApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBookApp.Business.Validators
+{
+    public class ContactValidator : AbstractValidator<Contact>
+    {
+        public ContactValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotNull()
+                .NotEmpty()
+                .EmailAddress()
+                .WithMessage("Please do not leave your E-mail blank!!")
+                .When(x => !string.IsNullOrEmpty(x.Email));
+            RuleFor(x => x.Email)
+                .MaximumLength(50)
+                .Length(3, 30)
+                .WithMessage("Please do not enter your E-mail more than 50 characters......");
+
+            RuleFor(x => x.PhoneNumber)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Please do not leave your Phone Number blank!!");                
+            RuleFor(x => x.PhoneNumber)
+                .MaximumLength(100)                
+                .WithMessage("Please do not enter your Phone Number more than 10 characters and less then 10......");
+
+            RuleFor(x => x.Address)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Please do not leave your Address blank!!");                
+            RuleFor(x => x.Address)
+                .MaximumLength(50)
+                .Length(3, 60)
+                .WithMessage("Please do not enter your Address more than 60 characters......");
+
+            RuleFor(x => x.City)
+               .NotNull()
+               .NotEmpty()
+               .WithMessage("Please do not leave your City blank!!");               
+            RuleFor(x => x.Address)
+                .MaximumLength(20)
+                .Length(3, 20)
+                .WithMessage("Please do not enter your City more than 20 characters......");
+
+            RuleFor(x => x.Address)
+               .NotNull()
+               .NotEmpty()
+               .WithMessage("Please do not leave your Address blank!!");           
+            RuleFor(x => x.Address)
+                .MaximumLength(50)
+                .Length(3, 60)
+                .WithMessage("Please do not enter your Address more than 60 characters......");
+
+            RuleFor(x => x.District)
+               .NotNull()
+               .NotEmpty()
+               .WithMessage("Please do not leave your District blank!!");               
+            RuleFor(x => x.Address)
+                .MaximumLength(50)
+                .Length(3, 60)
+                .WithMessage("Please do not enter your District more than 60 characters......");
+        }
+    }
+}
--- PhoneBookApp/Startup.cs
+++ PhoneBookApp/Startup.cs
@@ -1,3 +1,4 @@
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,7 +29,10 @@ namespace PhoneBookApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddFluentValidation(fv => {
+                fv.RegisterValidatorsFromAssemblyContaining<Startup>();
+            });
+                
             services.AddSingleton<IPersonService, PersonService>();
             services.AddSingleton<IPersonRepository, PersonRepository>();
             services.AddSwaggerDocument();
