1bd173fda27cfd038f9d0bc00bba1bc2d1905af7
Cenk Öztürk <cenk.ozturk@mikro.com.tr>
2021-12-19 11:05:59 +0300

    added another layer (validation).

---
+++ PhoneBookApp.Validation/Validators/PersonValidator.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation;
+using PhoneBook.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBookApp.Business.Validators
+{
+    public class PersonValidator : AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {                 
+
+            RuleFor(x => x.FirstName)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Please do not leave your name blank!!");
+            RuleFor(x => x.FirstName)
+                .MaximumLength(30)
+                .Length(3, 30)
+                .WithMessage("Please do not enter your name more than 30 characters......");
+
+            RuleFor(x => x.LastName)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Please do not leave your surname blank!");
+            RuleFor(x => x.LastName)
+                .MaximumLength(20)
+                .Length(3, 30)
+                .WithMessage("Please do not enter your name surname than 20 characters......");
+
+            RuleFor(x => x.Company)
+               .NotNull()
+               .NotEmpty()
+               .WithMessage("Please do not leave your Company blank!!");
+            RuleFor(x => x.Company)
+                .MaximumLength(30)
+                .Length(3, 30)
+                .WithMessage("Please do not enter your name more than 30 characters......");
+
+        }
+       
+    }
+}
